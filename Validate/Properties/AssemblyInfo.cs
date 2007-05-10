536daca225a100b3c08c2eab8fdbf7eadb9570cb
andrew_jacobs <andrew_jacobs@f9e4c086-e717-0410-a497-f4739b3d3a8a>
2007-05-10 16:27:48 +0000

    Added missing AssemblyInfo.cs for Validate application.
    git-svn-id: https://fpml-toolkit.svn.sourceforge.net/svnroot/fpml-toolkit/C#/trunk@145 f9e4c086-e717-0410-a497-f4739b3d3a8a

---
+++ Validate/Properties/AssemblyInfo.cs
@@ -0,0 +1,73 @@
+// Copyright (C),2005-2006 HandCoded Software Ltd.
+// All rights reserved.
+//
+// This software is licensed in accordance with the terms of the 'Open Source
+// License (OSL) Version 3.0'. Please see 'license.txt' for the details.
+//
+// HANDCODED SOFTWARE LTD MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
+// SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, OR NON-INFRINGEMENT. HANDCODED SOFTWARE LTD SHALL NOT BE
+// LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+// OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly: AssemblyTitle("HandCoded FpML Validation Utility")]
+[assembly: AssemblyDescription("Standalone validation tool")]
+[assembly: AssemblyConfiguration("Open Source Version")]
+[assembly: AssemblyCompany("HandCoded Software Ltd")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("(C)2006-2007 HandCoded Software Ltd.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]		
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.*")]
+
+//
+// In order to sign your assembly you must specify a key to use. Refer to the 
+// Microsoft .NET Framework documentation for more information on assembly signing.
+//
+// Use the attributes below to control which key is used for signing. 
+//
+// Notes: 
+//   (*) If no key is specified, the assembly is not signed.
+//   (*) KeyName refers to a key that has been installed in the Crypto Service
+//       Provider (CSP) on your machine. KeyFile refers to a file which contains
+//       a key.
+//   (*) If the KeyFile and the KeyName values are both specified, the 
+//       following processing occurs:
+//       (1) If the KeyName can be found in the CSP, that key is used.
+//       (2) If the KeyName does not exist and the KeyFile does exist, the key 
+//           in the KeyFile is installed into the CSP and used.
+//   (*) In order to create a KeyFile, you can use the sn.exe (Strong Name) utility.
+//       When specifying the KeyFile, the location of the KeyFile should be
+//       relative to the project output directory which is
+//       %Project Directory%\obj\<configuration>. For example, if your KeyFile is
+//       located in the project directory, you would specify the AssemblyKeyFile 
+//       attribute as [assembly: AssemblyKeyFile("..\\..\\mykey.snk")]
+//   (*) Delay Signing is an advanced option - see the Microsoft .NET Framework
+//       documentation for more information on this.
+//
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
+[assembly: ComVisibleAttribute(false)]
