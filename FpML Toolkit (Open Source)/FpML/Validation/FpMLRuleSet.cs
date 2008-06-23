f92fa06a70aee1f50adbb7c607aa0f9ce5485e2f
andrew_jacobs <andrew_jacobs@f9e4c086-e717-0410-a497-f4739b3d3a8a>
2008-06-23 22:29:46 +0000

    Added missing file.
    git-svn-id: https://fpml-toolkit.svn.sourceforge.net/svnroot/fpml-toolkit/C#/trunk@305 f9e4c086-e717-0410-a497-f4739b3d3a8a

---
+++ FpML Toolkit (Open Source)/FpML/Validation/FpMLRuleSet.cs
@@ -0,0 +1,49 @@
+// Copyright (C),2005-2008 HandCoded Software Ltd.
+// All rights reserved.
+//
+// This software is licensed in accordance with the terms of the 'Open Source
+// License (OSL) Version 3.0'. Please see 'license.txt' for the details.
+//
+// HANDCODED SOFTWARE LTD MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE
+// SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE, OR NON-INFRINGEMENT. HANDCODED SOFTWARE LTD SHALL NOT BE
+// LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING
+// OR DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+
+using System;
+using System.Xml;
+
+using HandCoded.Finance;
+using HandCoded.Xml;
+
+namespace HandCoded.FpML.Validation
+{
+	/// <summary>
+	/// The <b>FpMLRuleSet</b> is the abstract base class used by all other
+	/// FpML rule sets.
+	/// </summary>
+	public abstract class FpMLRuleSet : Logic
+	{
+		/// <summary>
+		/// Extracts an <see cref="Interval"/> from the data stored below the
+		/// given context node.
+		/// </summary>
+		/// <param name="context">The context <see cref="XmlElement"/>.</param>
+		/// <returns>An <see cref="Interval"/> constructed from the stored data.</returns>
+		protected static Interval ToInterval (XmlElement context)
+		{
+			if (context != null) {
+				try {
+					return (new Interval (
+						ToInteger (XPath.Path (context, "periodMultiplier")),
+						Period.ForCode (ToString (XPath.Path (context, "period")))));
+				}
+				catch (Exception) {
+					return (null);
+				}
+			}
+			return (null);
+		}
+	}
+}
