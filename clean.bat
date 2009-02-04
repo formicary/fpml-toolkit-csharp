7687afd8586c7b2d14c0df0e8dede9d8e4628ffe
andrew_jacobs <andrew_jacobs@f9e4c086-e717-0410-a497-f4739b3d3a8a>
2009-02-04 14:37:46 +0000

    More cleaning up
    git-svn-id: https://fpml-toolkit.svn.sourceforge.net/svnroot/fpml-toolkit/C#/trunk@378 f9e4c086-e717-0410-a497-f4739b3d3a8a

--- clean.bat
+++ clean.bat
@@ -1,5 +1,8 @@
-RMDIR /Q /S HandCoded\bin
 RMDIR /Q /S Validate\bin
+RMDIR /Q /S Validate\obj
 RMDIR /Q /S Classify\bin
+RMDIR /Q /S Classify\obj
+RMDIR /Q /S FpML Toolkit\bin
+RMDIR /Q /S FpML Toolkit\obj
 RMDIR /Q /S doc
 RMDIR /Q /S dist
